ed93ad384b0dcc90725315f00e443a110da09a89
s4eka <36367218+s4eka@users.noreply.github.com>
2018-02-11 18:24:27 +0300

    Enttity
    +Motherboard

---
+++ ComputerMaker/Models/Motherboad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerMaker.Models
+{
+    public class Motherboad
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Socet { get; set; }
+        public string Formfactor { get; set; }
+        public string MemoryType { get; set; }
+        public int MemorySlots { get; set; }
+        public int MaxMemoryCapacity { get; set; }
+        public int MaxFrequency { get; set; }
+        public int SATA { get; set; }
+        public int PCIe { get; set; }
+        public bool IntegratedGraphics { get; set; }
+
+    }
+}
