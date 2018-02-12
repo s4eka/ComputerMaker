a105266d6820632303b96e9aa1d4cb6484097e32
s4eka <36367218+s4eka@users.noreply.github.com>
2018-02-12 17:31:37 +0300

    proc and mother
    finally this is working

--- ComputerMaker/Controllers/HomeController.cs
+++ ComputerMaker/Controllers/HomeController.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Script.Serialization;
 using System.Web;
 using System.Web.Mvc;
 using ComputerMaker.Enumerations;
 using ComputerMaker.Models;
+using Newtonsoft.Json;
 
 namespace ComputerMaker.Controllers
 {
     public class HomeController : Controller
     {
 
-        ComputerContext db = new ComputerContext();
+        static ComputerContext db = new ComputerContext();
+        static List<Motherboard> mbList = new List<Motherboard>();
+        static List<Processor> procList = new List<Processor>();
+
+        static Computer computer = new Computer();
+
         Socet sc = new Socet();
         FormFactor ff = new FormFactor();
         MemoryType mt = new MemoryType();
         Interfaces If = new Interfaces();
         
+        public static void LoadFromDB()
+        {
+            mbList = db.Motherboards.ToList();
+            procList = db.Processors.ToList();
+        }
 
         public ActionResult Index()
         {
@@ -43,7 +55,7 @@ namespace ComputerMaker.Controllers
         [HttpGet]
         public ActionResult GetMotherboardList()
         {
-            IEnumerable<Motherboard> mb = db.Motherboards;
+            IEnumerable<Motherboard> mb = mbList;
             return View(mb);
         }
 
@@ -67,28 +79,91 @@ namespace ComputerMaker.Controllers
         [HttpGet]
         public ActionResult GetProcessorList()
         {
-            IEnumerable<Processor> pr = db.Processors;
+            IEnumerable<Processor> pr = procList;
             return View(pr);
         }
+
+
+
+
+
         [HttpGet]
         public ActionResult Maker()
         {
-            ViewBag.Motherboards = db.Motherboards;
+            Reload();
             return View();
         }
-        [HttpPost]
+
         public ActionResult MakerPost()
         {
-            return View("Index");
+
+            ViewBag.JSONMotherboards = JsonConvert.SerializeObject(mbList);
+            return View();
         }
 
-        [HttpGet]
-        public ActionResult LoadProcessors(string Id)
+        public void CheckMotherboard(string Id)
         {
             int id = Int32.Parse(Id);
-            var socet = db.Motherboards.Find(id).Socet;
-            var Processors = db.Processors.Where(a => a.Socet == socet).ToList();
-            return PartialView(Processors);
+            computer.MotherboardId = id;
+        }
+
+        public void CheckProcessor(string Id)
+        {
+            int id = Int32.Parse(Id);
+            computer.ProcessorId = id;
+        }
+
+        public void CheckRAM(string Id)
+        {
+            int id = Int32.Parse(Id);
+            computer.RAMId = id;
+        }
+
+        
+        public ActionResult LoadProcessors()
+        {
+            int MotherId = computer.MotherboardId;
+            List<Processor> proc = new List<Processor>();
+            ViewBag.computer = computer;
+            if (MotherId == 0)
+            {
+                proc = procList;
+            }
+            else
+            {
+                var socet = mbList.Find(a => a.Id == MotherId).Socet;
+                proc = procList.Where(a => a.Socet == socet).ToList();
+            }
+            return PartialView(proc);
+        }
+
+        public ActionResult LoadMotherboard()
+        {
+            int ProcId = computer.ProcessorId;
+            List<Motherboard> mother = new List<Motherboard>();
+            ViewBag.computer = computer;
+            if(ProcId == 0)
+            {
+                mother = mbList;
+            }
+            else
+            {
+                var socet = procList.Find(a => a.Id == ProcId).Socet;
+                mother = mbList.Where(a => a.Socet == socet).ToList();
+            }
+            return PartialView(mother);
+        }
+        public void Reload()
+        {
+            LoadMotherboard();
+            LoadProcessors();
+        }
+
+        public void Reload(string motherId, string procId)
+        {
+            CheckMotherboard(motherId);
+            CheckProcessor(procId);
+            Reload();
         }
     }
 }
---
+++ ComputerMaker/Models/Computer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerMaker.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public int MotherboardId { get; set; }
+        public int ProcessorId { get; set; }
+        public int RAMId { get; set; }
+    }
+}
